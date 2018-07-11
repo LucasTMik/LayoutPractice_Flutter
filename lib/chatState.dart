70f79443a01315ccface1e3e9ad9d1daca34bf8f
Lucas <lucascercun@gmail.com>
2018-07-11 16:46:25 -0300

    first commit

---
+++ lib/chatState.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class ChatList extends StatefulWidget {
+  @override
+  createState() => ChatListState();
+}
+
+class ChatListState extends State<ChatList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: ListView(
+          children: [
+
+          ]
+        ),
+      ),
+    );
+  }
+
+  Widget chat = Container(
+    
+  );
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:badge/badge.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'WhatsPop',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+        appBar: AppBar(
+            title: Text('WhatsPop'),
+            actions: <Widget>[
+          
+              IconButton(
+                onPressed: null,
+                icon: Icon(Icons.search),
+                tooltip: 'search',
+              ),
+              IconButton(
+                onPressed: null,
+                icon: Icon(Icons.more_vert),
+                tooltip: 'menu',
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(icon:Icon(Icons.camera_alt)),
+                Tab(text: 'CHATS'),
+                Tab(text: 'STATUS'),
+                Tab(text: 'CALLS'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Container(color:Colors.black),
+              ChatList(),
+              Text(''),
+              Text(''),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            tooltip: 'newChat',
+            onPressed: null,
+            child: Icon(Icons.chat),
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
+
+class ChatList extends StatefulWidget {
+  @override
+  createState() => ChatListState();
+}
+
+class ChatListState extends State<ChatList> {
+
+  List<Widget> chatList;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+
+        
+      ),
+    );
+  }
+
+  Widget chat = Container(
+
+  );
+}
+
